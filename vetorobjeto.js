a7549111a1be0fba7a6b6369866bb160dde0044a
FernandoTavares8 <fernandotnominato@gmail.com>
2019-08-21 22:05:38 -0300

    Exemplo de vetor de objeto v 1.0

---
+++ vetorobjeto.js
@@ -0,0 +1,18 @@
+function funcao(){
+   
+    var vet = []
+
+    for (var i=0; i<5; i++){
+        var objeto = {}
+        // coloca os dados no objeto
+        objeto.codigo = Number(prompt("Código"))
+        objeto.nome = prompt("Nome")
+        objeto.quantidade = Number(prompt("Quantidade"))
+        objeto.preco = Number(prompt("Preço"))
+    
+    // coloca os dados no vetor
+    vet.push(objeto)
+    
+    }
+
+}
